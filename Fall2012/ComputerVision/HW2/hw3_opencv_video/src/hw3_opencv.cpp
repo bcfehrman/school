85264c3282a4d8adda085554a45069e959dd1ccb
Brian Fehrman <bcfehrman@gmail.com>
2012-09-18 12:11:01 -0600

    Worked more on the Computer Vision HW3 openCV video implementation of the SVD algoritm. It captures a frame, converts to 32F, converts to gray, then performs the SVD on the gray frame. It then displays windows for each desired lower rank representation of the image. Can handle any desired number of lower rank representations which is currently specified in the code. Right now it runs really slow at about 8FPS. Thinking it is mostly my POS laptop. Will try running on a faster machine. Will also try running with a different camera

--- Fall2012/ComputerVision/HW2/hw3_opencv_video/src/hw3_opencv.cpp
+++ Fall2012/ComputerVision/HW2/hw3_opencv_video/src/hw3_opencv.cpp
@@ -6,43 +6,103 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <ctime>
+
+#define WINDOW_SIZE_CHOICE CV_WINDOW_NORMAL
 
 using namespace cv;
 using namespace std;
 
+struct xyPos
+{
+	int x;
+	int y;
+};
+
 int main( int argc, char *argv[])
 {	
-	Mat frame_gray;
-	Mat w, u, vt;
+	int numFrames = 6;
+	int rankStep = 5;
+	string frameNames[numFrames];
         VideoCapture cap(0); //open the default camera
-	Mat imnew;
+	Mat frame_lower_rank[numFrames];
+	ostringstream convert;
+	int currRank = rankStep;
+	int i = 0;
+	int frameWidth = 160;
+	int frameHeight = 120;
+	xyPos windowPositions[numFrames];
+	string startName = "Rank ";
+	clock_t begin;
+	clock_t end;
+	double timeDiff = 0;
 
 	if(!cap.isOpened()) //check if we succeeded
 		return -1;
 
+	for(i = 0; i < numFrames; i++)
+	{
+		convert << i * rankStep + rankStep;
+		frameNames[i] = startName + convert.str();
+		namedWindow(frameNames[i], WINDOW_SIZE_CHOICE);
+		convert.clear();
+		convert.str("");		
 
-	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
+		windowPositions[i].x = (i % 2) * frameWidth * 2.5 + 100;
+		windowPositions[i].y = i / 2 * frameHeight * 2.5;
+	}
+
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, frameWidth);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, frameHeight);
+
+	namedWindow("Non SVD", WINDOW_SIZE_CHOICE);
 
 	for(;;)
 	{
+		begin = clock();
+		
 		Mat frame_bgr;
 		
 		cap >> frame_bgr;
-
-		//Convert to 32bit float
-		frame_bgr.convertTo(frame_bgr, CV_32F, 1/255.0);
+	
+		frame_bgr.convertTo(frame_bgr, CV_32F, 1/255.0);	
+		
 		//Convert to gray scale
-		cvtColor(frame_bgr, frame_gray, CV_BGR2GRAY);	
+		cvtColor(frame_bgr, frame_bgr, CV_BGR2GRAY);
+		imshow("Non SVD", frame_bgr);
+		cvMoveWindow("Non SVD", 900, 0);
+
+	//	cout << frame_bgr.rows << " " << frame_bgr.	
+		
+	//	reduced_frame_bgr = reduceFrameResolution(frame_bgr, 0.9f);
 
 		//Perform the SVD that gives the U, W, Vt (or U, S, Vt) except W is just a vector of the singular values
-		SVD svd(frame_gray);
+		SVD svd(frame_bgr);
+		
+		currRank = rankStep;
+		
+		for(i = 0; i < numFrames; i++)
+		{
+			//Reconstruct the iamge, the W vector is converted into a diagonal matrix so that it plays nicely, might be able to make that better
+			frame_lower_rank[i] = svd.u(Range::all(), Range(0,currRank)) * 
+				Mat::diag(svd.w)(Range(0,currRank),Range(0,currRank)) *  
+				svd.vt(Range(0,currRank), Range::all());
 
-		//Reconstruct the iamge, the W vector is converted into a diagonal matrix so that it plays nicely, might be able to make that better
-		//imnew = svd.u(Range::all(), Range(0,5)) * Mat::diag(svd.w)(Range(0,5),Range(0,5)) *  svd.vt(Range(0,5), Range::all());
+			currRank += rankStep;
+		}
 
 		//namedWindow("Gray window", CV_WINDOW_AUTOSIZE);
-		imshow("Display window", frame_gray);
+		for(i = 0; i < numFrames; i++)
+		{
+			imshow(frameNames[i], frame_lower_rank[i]);
+			cvMoveWindow( frameNames[i].c_str(), windowPositions[i].x, windowPositions[i].y); 
+		}			
 
+		end = clock();
+		timeDiff = (double)(end - begin);
+	//	cout << 1.0 / (timeDiff / CLOCKS_PER_SEC ) << endl;
 		if(waitKey(30) >= 0) break;
 	}
 

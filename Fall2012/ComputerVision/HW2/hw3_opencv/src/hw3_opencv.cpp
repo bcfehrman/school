1543589f2ceb41bc57f22200911e42bc1b6f8341
Brian Fehrman <bcfehrman@gmail.com>
2012-09-17 20:10:23 -0600

    Got the HW3 for computer vision to work with OpenCV C++. It reads an image in, converts to 32bit Float, converts to gray scale, performs the SVD on the image, then reconstructs and displays the original image with a lower rank representation

--- Fall2012/ComputerVision/HW2/hw3_opencv/src/hw3_opencv.cpp
+++ Fall2012/ComputerVision/HW2/hw3_opencv/src/hw3_opencv.cpp
@@ -13,7 +13,6 @@ int main( int argc, char *argv[])
 	Mat im1_rgb;
 	Mat im1_gray;
 	Mat w, u, vt;
-	int height;
 
 	if(argc < 2)
 	{
@@ -29,25 +28,19 @@ int main( int argc, char *argv[])
 		exit(0);
 	}
 
-	//Get the image data
-	height = im1_rgb.rows;
-	//width = i;
-	//step = img1->widthStep;
-	//channels = img->nChannels;
-	//data = (uchar *)img->imageData;
-	//printf("Processing a %d%d image with %d channels \n", height, width;
-
 	//Convert to 32bit
 	im1_rgb.convertTo(im1_rgb, CV_32F, 1/255.0);
 	
 	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
 	imshow("Display window", im1_rgb);
 
+	//Convert to gray scale
 	cvtColor(im1_rgb, im1_gray, CV_RGB2GRAY);	
 
 	namedWindow("Gray window", CV_WINDOW_AUTOSIZE);
 	imshow("Gray window", im1_gray);
 
+	//Perform the SVD that gives the U, W, Vt (or U, S, Vt) except W is just a vector of the singular values
 	SVD svd(im1_gray);
 
 	cout << svd.u.rows << " " << svd.u.cols << endl;
@@ -56,12 +49,12 @@ int main( int argc, char *argv[])
 
 	cout << svd.vt.rows << " " << svd.vt.cols << endl;
 
-	Mat imnew = svd.u.rowRange(0,5) * Mat::diag(svd.w).rowRange(0,5) *  svd.vt.rowRange(0,5);
+	//Reconstruct the iamge, the W vector is converted into a diagonal matrix so that it plays nicely, might be able to make that better
+	Mat imnew = svd.u(Range::all(), Range(0,5)) * Mat::diag(svd.w)(Range(0,5),Range(0,5)) *  svd.vt(Range(0,5), Range::all());
 
-	
-	//multiply(svd.u, svd.w, imnew);
-	//multiply(imnew, vt, imnew);
-	namedWindow("Reconstruct", CV_WINDOW_AUTOSIZE);
+	cout << imnew.rows << " " << imnew.cols << endl;	
+
+	namedWindow("Reconstruct", CV_WINDOW_NORMAL);
 	imshow("Reconstruct", imnew);
 
 	waitKey(0);
